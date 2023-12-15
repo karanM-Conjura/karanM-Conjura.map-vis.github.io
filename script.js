e72afd093e24c5ddd23cc5a8ca83d34ead452f88
karanM-Conjura <karan.manhas@conjura.com>
2023-12-15 10:35:40 +0000

    add map to file

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
 alert('Hello from Conjura!');
+
+// Mapbox logic
+
+mapboxgl.accessToken = 'pk.eyJ1Ijoia2FyYW4tYzAiLCJhIjoiY2xxNmd1ZGdrMHIwMTJrcGFkYWxzbWE2NyJ9.W1usXxcSztAC5kqG-GqQOw';
+const map = new mapboxgl.Map({
+container: 'map',
+// Choose from Mapbox's core styles, or make your own style with Mapbox Studio
+style: 'mapbox://styles/mapbox/light-v11', // light map background but can be changed here
+center: [0, 0], // longitude and latitude 
+zoom: 0.4,
+projection: 'naturalEarth' // starting projection
+});
